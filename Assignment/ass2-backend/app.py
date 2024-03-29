7b653953974a63a2a75e23ecda6a1abd3d0d4304
Code Help Tutor <1823890830@qq.com>
2024-03-29 03:26:29 +0000

    WeChat cstutorcs COMP6080 Web Front End Programming

---
+++ Assignment/ass2-backend/app.py
@@ -0,0 +1,13 @@
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+from flask import Flask
+from flask_restplus import Api
+from flask_cors import CORS
+from util.DB_Interface import DB
+
+app = Flask(__name__)
+app.config['ERROR_404_HELP'] = False
+CORS(app)
+api = Api(app)
+db = DB()
